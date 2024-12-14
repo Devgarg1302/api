4dcdf7265888327b3f8d3c86740451a8828a2474
Dev Garg <devindergarg1302@gmail.com>
2024-12-14 22:31:38 +0530

    commit

--- index.js
+++ index.js
@@ -21,30 +21,30 @@ async function connectToDatabase()
 
 connectToDatabase();
 
+// app.get('/', async (req, res) => {
+//     try {
+//         const database = client.db('scheduleDB');
+//         const collection = database.collection('timeData');
+
+//         const doc = {
+//             "0": ["09:45 14-12-2024", "13:45 14-12-2024", "20:45 14-12-2024"],
+//             "1": ["09:45 14-12-2024", "13:45 14-12-2024", "20:45 14-12-2024"],
+//             "2": ["09:45 14-12-2024", "13:45 14-12-2024", "20:45 14-12-2024"],
+//             "3": ["09:45 14-12-2024", "13:45 14-12-2024", "20:45 14-12-2024"],
+//             "4": ["09:45 14-12-2024", "13:45 14-12-2024", "20:45 14-12-2024"],
+//             "5": ["09:45 14-12-2024", "13:45 14-12-2024", "20:45 14-12-2024"],
+//             "6": ["09:45 14-12-2024", "13:45 14-12-2024", "20:45 14-12-2024"],
+//         };
+
+//         const result = await collection.insertOne(doc);
+//         console.log(`New document inserted with _id: ${result.insertedId}`);
+
+//     } catch (error) {
+//         res.status(500).send(error.message);
+//     }
+// });
+
 app.get('/', async (req, res) => {
-    try {
-        const database = client.db('scheduleDB');
-        const collection = database.collection('timeData');
-
-        const doc = {
-            "0": ["09:45 14-12-2024", "13:45 14-12-2024", "20:45 14-12-2024"],
-            "1": ["09:45 14-12-2024", "13:45 14-12-2024", "20:45 14-12-2024"],
-            "2": ["09:45 14-12-2024", "13:45 14-12-2024", "20:45 14-12-2024"],
-            "3": ["09:45 14-12-2024", "13:45 14-12-2024", "20:45 14-12-2024"],
-            "4": ["09:45 14-12-2024", "13:45 14-12-2024", "20:45 14-12-2024"],
-            "5": ["09:45 14-12-2024", "13:45 14-12-2024", "20:45 14-12-2024"],
-            "6": ["09:45 14-12-2024", "13:45 14-12-2024", "20:45 14-12-2024"],
-        };
-
-        const result = await collection.insertOne(doc);
-        console.log(`New document inserted with _id: ${result.insertedId}`);
-
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-});
-
-app.get('/data', async (req, res) => {
     try {
         const database = client.db('scheduleDB');
         const collection = database.collection('timeData');
